877d973ae58528f2390838574d1fce16baacd45e
Your Name <almannofrancesco@gmail.com>
2023-01-25 16:14:35 +0100

    components AppContent in progress with data

--- src/components/AppContent.vue
+++ src/components/AppContent.vue
@@ -1,5 +1,41 @@
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      titoloJumbo: {
+        titolo: "MAKE A DIFFERENCE",
+        sottotitolo:
+          "As long as poverty, injustice & inequality persist, none of us can truly rest",
+      },
+      titoloParagrago: {
+        titolo: "EVERY MOMENT COUNTS",
+        sottotitolo:
+          "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.",
+      },
+      titoloValori: {
+        titolo: "OUR VALUES",
+        sottotitolo: "Think Big. Act Bigger.",
+      },
+      titoloCause: {
+        titolo: "RECENT CAUSES",
+        sottotitolo: "We run projects in over 30 countries in 5 continents",
+      },
+      titoloUltimiArticoli: {
+        titolo: "LATEST ARTICLES",
+        sottotitolo: "Togheter we make all the difference",
+      },
+      titoloVista: {
+        titolo: "VIEW ALL OUR ARTICLES",
+        sottotitolo: "",
+      },
+      titoloUltimoParagrafo: {
+        titolo: "CHANGE A LIFE TODAY",
+        sottotitolo:
+          "As long as poverty, injustice & inequality persist, none of us can truly rest. It doesnt't take much to change a life. Get in touch today and start making the difference.",
+      },
+    };
+  },
+};
 </script>
 
 
