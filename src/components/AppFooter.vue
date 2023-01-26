7920a7b210bdabbc3da8a30eeee0f4c8b49b3cc2
Your Name <almannofrancesco@gmail.com>
2023-01-26 12:44:55 +0100

    footer in progress

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,11 +1,23 @@
 <script>
-export default {};
+export default {
+  props: {
+    footerSection: Array,
+  },
+};
 </script>
 
 
 <template lang="">
-  <div>
-    
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img src="../assets/images/HelloIMG1669149322752.jpeg" alt="">
+        <hr>
+        <p>As long as poverty, injustice & inequality persist, none of us can truly rest. It doesn't take much to change a life. Get in touch today and start making the difference.</p>
+        <button>VOLUNTEER</button>
+        <button>DONATE NOW</button>
+      </div>
+    </div>
   </div>
 </template>
 
