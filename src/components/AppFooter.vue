d44965e62dd18bf374606754fe30e9a1e11b0228
Your Name <almannofrancesco@gmail.com>
2023-01-26 12:52:40 +0100

    footer in progress

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -8,7 +8,7 @@ export default {
 
 
 <template lang="">
-  <div class="container">
+  <div class="container-footer1">
     <div class="row">
       <div class="col">
         <img src="../assets/images/HelloIMG1669149322752.jpeg" alt="">
@@ -19,9 +19,35 @@ export default {
       </div>
     </div>
   </div>
+  <div class="container-footer2">
+        <div class="row-footer2">
+            <div class="par">
+                <p><i class="fa-regular fa-copyright"></i> Copyright 2012 - 2022 | Avada Theme by <a href="">ThemeFusion</a> | All Rights Reserved | Powered by <a href="">Wordpress</a></p>
+            </div>
+            <div class="icons">
+                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-instagram"></i>
+    
+            </div>
+        </div>
+    </div>
 </template>
 
 
 
-<style lang="">
+<style lang="scss">
+.container-footer1 {
+  margin-top: 500px;
+}
+hr {
+  width: 250px;
+  color: yellow;
+  margin: 0 auto;
+}
+.container-footer1 img {
+  width: 100%;
+  height: 600px;
+}
 </style>
